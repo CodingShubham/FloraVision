cc661942c5c02f4f62df82e0189996016f2dbbb0
Shubham <ss9824473@gmail.com>
2026-05-13 18:41:34 +0530

    Created Small ReviewCard in Hero Section

--- app/src/Hero.jsx
+++ app/src/Hero.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { FaPlay } from "react-icons/fa";
 import HeroPot from "./assets/images/HeroPot.png"
 import { IoIosArrowForward } from "react-icons/io";
+import HeroProfile from "./assets/images/HeroProfile.png"
+import { IoStar } from "react-icons/io5";
+import { LiaStarHalfSolid } from "react-icons/lia";
 function Hero() {
   return (
     <div className='  flex justify-between relative overflow-hidden  '>
@@ -15,11 +18,31 @@ function Hero() {
     <p className='font-indie'>Live Demo...</p>
 </div>
 
+<div className='  p-6 relative  w-[310px] h-[180px] shadow-lg  bg-white/5 border border-r-0  border-b-0 border-gray-400   rounded-b-[40px] mr-10 mt-28 rounded-t-[40px] backdrop-blur-lg'>
+
+<div className='flex gap-6'>
+<img className='w-12 h-12 object-cover rounded-full' src={HeroProfile} alt="profile" />
+<div>
+  <span>Ronnie Hamill</span>
+<div className='text-yellow-300 flex text-xs gap-1.5'>
+<IoStar />  
+<IoStar />
+<IoStar />
+<IoStar />
+<LiaStarHalfSolid  />
+</div>
+</div>
+</div>
+
+<p className='pt-4 text-sm text-gray-300 flex break-words '>I can't express how thrilled I am with my new natural plants! They bring such a fresh and vibrant energy to my home.</p>
+
+</div>
+
     </div>
 
 <div  className=' relative overflow-visible  w-[380px] h-[450px] shadow-lg  bg-white/5 border border-t-0  rounded-b-[40px] mr-10 mt-20 rounded-t-[40px] backdrop-blur-lg '>
 
-    <img className=' object-fit absolute -top-[170px]  ' src={HeroPot} alt="" />
+    <img className='  absolute -top-[170px]  ' src={HeroPot} alt="" />
 
     <div className=' pt-[220px] text-gray-300 flex flex-col  pl-16'>
       <p className=''>Indoor Plant </p>
@@ -31,6 +54,7 @@ function Hero() {
     </div>
 
 </div>
+
     </div>
   )
 }
